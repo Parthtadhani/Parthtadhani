e6eb6a5cce46ccebd971b3fe847f869b6559fcc5
kryzetech <123tadhani@gmail.com>
2023-06-07 15:37:49 +0530

    text1

---
+++ lib/demo2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class demo2 extends StatefulWidget {
+  const demo2({super.key});
+
+  @override
+  State<demo2> createState() => _demo2State();
+}
+
+class _demo2State extends State<demo2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+
+    );
+  }
+}
