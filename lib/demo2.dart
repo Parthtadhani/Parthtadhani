052396102f2bf5c071934c1f65f0d8dcc3ea36b0
kryzetech <123tadhani@gmail.com>
2023-06-07 17:06:36 +0530

    text2

--- lib/demo2.dart
+++ lib/demo2.dart
@@ -10,6 +10,6 @@ class demo2 extends StatefulWidget {
 class _demo2State extends State<demo2> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,121 @@
+import 'dart:math';
+
+import 'package:d_chart/d_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+import 'demo2.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home screen'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-
-    setState(() {
-      // Navigator.push(
-      //     context,
-      //     MaterialPageRoute(
-      //       builder: (context) => const Demo(),
-      //     ));
-      _counter= _counter+1;
-    });
-  }
+class Home extends StatelessWidget {
+  final dataMap = <String, double>{
+    "Flutter": 5,
+    "flutter": 5,
+  };
+  final colorList = <Color>[
+    Colors.greenAccent,
+    Colors.amber,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
+      appBar: AppBar(title: Text('D\'Chart')),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: AspectRatio(
+                aspectRatio: 12 / 7,
+                child: InkWell(
+                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => demo2(),)),
+                  child: DChartBar(
+                    data: [
+                      {
+                        'id': "12",
+                        'data': [
+                          {'domain': '2020', 'measure': 3},
+                          {'domain': '2021', 'measure': 4},
+                          {'domain': '2022', 'measure': 6},
+                          {'domain': '2023', 'measure': 1.3},
+                        ],
+                      },
+                    ],
+                    domainLabelPaddingToAxisLine: 16,
+                    axisLineTick: 2,
+                    axisLinePointTick: 2,
+                    axisLinePointWidth: 10,
+                    axisLineColor: Colors.green,
+                    measureLabelPaddingToAxisLine: 16,
+                    barColor: (barData, index, id) => Colors.green,
+                    showBarValue: true,
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: DChartPie(
+                  data: [
+                    {'domain': 'Flutter', 'measure': 28},
+                    {'domain': 'React Native', 'measure': 27},
+                    {'domain': 'Ionic', 'measure': 20},
+                    {'domain': 'Cordova', 'measure': 15},
+                  ],
+                  fillColor: (pieData, index) {
+                    switch (pieData['domain']) {
+                      case 'Flutter':
+                        return Colors.blue;
+                      case 'React Native':
+                        return Colors.blueAccent;
+                      case 'Ionic':
+                        return Colors.lightBlue;
+                      default:
+                        return Colors.orange;
+                    }
+                  },
+                  pieLabel: (pieData, index) {
+                    return "${pieData['domain']}:\n${pieData['measure']}%";
+                  },
+                  labelPosition: PieLabelPosition.outside,
+                  donutWidth: 20,
+                ),
+              ),
             ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: PieChart(
+                dataMap: dataMap,
+                chartType: ChartType.ring,
+                baseChartColor: Colors.grey[500]!.withOpacity(0.15),
+                colorList: colorList,
+                chartValuesOptions: ChartValuesOptions(
+                  showChartValuesInPercentage: true,
+                ),
+                totalValue: 20,
+              ),
+            ),
+            const SizedBox(height: 50),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
